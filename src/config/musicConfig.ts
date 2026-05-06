9922e11a594240d4eba28b722ef9d00b9380676e
*a <2140274627@qq.com>
2026-05-06 17:58:24 +0800

    更新内容

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -54,7 +54,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 				artist: "",
 				url: "assets/music/a.mp3",
 				cover: "assets/music/cover/music.jpg",
-				// lrc: "assets/music/lrc/aa.lrc?raw",
+				lrc: "assets/music/lrc/a.lrc",
 			},
 		],
 	},
